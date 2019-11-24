34ae935d93aa6a829e20388b218fcb49822a9444
mreveret <51386677+mreveret@users.noreply.github.com>
2019-11-24 14:07:23 +0100

    Update filler.h

--- filler.h
+++ filler.h
@@ -10,47 +10,49 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef filler_h
+#ifndef FILLER_H
+# define FILLER_H
 
-# define filler_h
-# include "libft/libft.h"
+# include <unistd.h>
 # include <stdio.h>
+# include <stdlib.h>
+# include "libft/libft.h"
 
-typedef struct	s_pivot
+typedef struct	s_struct
 {
-	int			x;
-	int			y;
-}				t_pivot;
-
-typedef struct	s_piece
-{
-	int			hp;
-	int			wp;
-	char		**tab2;
-	t_pivot		*p;
-}				t_piece;
-
-typedef struct	s_env
-{
-	char		**tab1;
-	char		**tab2;
-	int			h;
-	int			w;
-	int			hp;
-	int			wp;
+	int			a;
 	int			p;
+	char			m;
+	char			**tab1;
+	int			l;
 	int			c;
+	int			heigth_p;
+	int			width_p;
+	int			height_f;
+	int			width_f;
 	int			x;
 	int			y;
-	t_pivot		*e;
-}				t_env;
+	char			**tab2;
+	int			f;
+	int			f2;
+	int			x1;
+	int			x2;
+	int			y1;
+	int			y2;
+	int			tmp;
+	int			tmp2;
+	int			i;
+	int			j;
+	int			i1;
+	int			j1;
+}				t_struct;
 
-	
-static void		print_words_tables(char **tab, int si);
-void			filler(t_env *f);
-static void		init(t_env *f);
-static void		tab_opti2(t_env *f);
-void			tab_opti(t_env *f);
-int				check_piece(t_env *f);
+int				main(void);
+int				filler(t_struct *f);
+void			tab_opti(t_struct *f);
+int				check_pieces(t_struct *f, int **tab);
+void			map(t_struct *f, int i, int j, int **tab);
+int				findone(char c, char *s);
+void			tabinit(t_struct *f, int **tab);
+
 #endif
-
