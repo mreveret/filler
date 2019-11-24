b43510ff4cfdfb52cb791a31e2ed0e986c87d522
mreveret <51386677+mreveret@users.noreply.github.com>
2019-11-24 14:43:23 +0100

    Update filler.c

--- filler.c
+++ filler.c
@@ -11,44 +11,77 @@
 /* ************************************************************************** */
 
 #include "filler.h"
-
-static void print_words_tables(char **tab, int si)
+int             findone(char c, char *s)
+{
+    int i;
+    i = -1;
+    if (!(s) || !(c))
+        return (0);
+    while (s[++i])
+        if (s[i] == c)
+            return (1);
+    return (0);
+}
+static void     init2(t_struct *f, int **tab)
 {
     int i;
     int j;
     i = 0;
-    while (i < si)
-    {
-        j = 0;
-        while (tab[i][j])
-        {
-            ft_putchar(tab[i][j]);
-            j++;
-        }
-        ft_putchar('\n');
-        i++;
-    }
-}
-static void init(t_env *f)
-{
-    int     i;
-    int     j;
-    i = -1;
     while (++i < f->h + 1)
     {
-        j = -1;
-        while (f->tab1[i][++j])
-            if (f->tab1[i][j] == f->c)
-                break ;
+        j = 3;
+        while (++j < f->w + 4)
+        {
+            if (f->p == 1)
+            {
+                if (findone(f->tab1[i][j], "xX") == 1)
+                    map(f, i, j, tab);
+            }
+            if (f->p == 2)
+            {
+                if (findone(f->tab1[i][j], "oO") == 1)
+                    map(f, i, j, tab);
+            }
+        }
     }
-    f->x = j;
-    f->y = i;
-    tab_opti(f);
 }
-void        filler(t_env *f)
+static void     init(t_struct *f)
 {
+    int     i;
+    int     **tab;
+    if (!(tab = (int **)malloc(sizeof(int *) * f->h)))
+        return ;
+    i = -1;
+    while (++i < f->h)
+        if (!(tab[i] = (int *)malloc(sizeof(int) * f->w)))
+            return ;
+    tabinit(f, tab);
+    init2(f, tab);
+    tab_opti(f);
+    check_pieces(f, tab);
+    i = -1;
+    while (++i < f->h)
+        free(tab[i]);
+    free(tab);
+}
+int             filler(t_struct *f)
+{
+    int i;
+    int tmp;
+    int tmp2;
+    tmp = f->h_p;
+    tmp2 = f->w_p;
     init(f);
-    check_piece(f);
-    print_words_tables(f->tab1, f->h + 1);
-    print_words_tables(f->tab2, f->hp);
+    tmp -= f->h_p;
+    tmp2 -= f->w_p;
+    ft_putnbr(f->h_f - 1 - tmp);
+    ft_putchar(' ');
+    ft_putnbr(f->w_f - 4 - tmp2);
+    ft_putchar('\n');
+    f->h = 0;
+    f->w = 0;
+    f->h_p = 0;
+    f->w_p = 0;
+    i = -1;
+    return (1);
 }
