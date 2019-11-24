f3e15563ec3c49de66e9b5808a8fa00ab338aea7
mreveret <51386677+mreveret@users.noreply.github.com>
2019-11-24 14:02:35 +0100

    Update check_pieces.c

--- check_pieces.c
+++ check_pieces.c
@@ -9,30 +9,84 @@
 /*   Updated: 2019/10/23 20:39:48 by mreveret         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 #include "filler.h"
 
-int		check_piece(t_env *f)
+static void		check2(t_struct *f, int **tab)
 {
-	int i;
-	int j;
-
-	i = 0;
-	if (f->x + (f->wp - f->e->x ) > f->w || f->x - (f->e->x) < 0)
-		return (0);
-	if (f->y + (f->hp - f->e->y ) > f->h || f->y - (f->e->y) < 0)
-		return (0);
-	while (i < f->h - f->y)
+	while (f->j < f->cp)
 	{
-		j = 0;
-		while (j < f->w -f-> x)
+		if (f->tab2[f->i][f->j] == '*')
 		{
-			if (f->tab1[f->y - i] [(f->x) + j] == 'X' ||f->tab1[f->y - i] [(f->x) + j] == 'O')
-				return (0);
-			j++;
+			if (f->y2 - 4 >= f->c || f->x2 - 1 >= f->l
+					|| tab[f->x2 - 1][f->y2 - 4] == -1)
+			{
+				f->tmp2 = -1;
+				f->tmp = 0;
+				return ;
+			}
+			else
+			{
+				f->tmp2 = 0;
+				f->tmp += tab[f->x2 - 1][f->y2 - 4];
+			}
 		}
-	i++;
+		f->y2++;
+		f->j++;
 	}
-	
+}
+
+static void		check1(t_struct *f, int **tab)
+{
+	while (f->i < f->lp)
+	{
+		f->j = 0;
+		f->y2 = f->y1;
+		check2(f, tab);
+		if (f->tmp2 == -1)
+		{
+			f->tmp2 = 0;
+			return ;
+		}
+		f->x2++;
+		f->i++;
+	}
+}
+
+static void		init(t_struct *f)
+{
+	f->i = 0;
+	f->j = 0;
+	f->x1 = 1;
+	f->tmp = 0;
+	f->tmp2 = 0;
+	f->a = 0;
+	f->lf = 0;
+	f->cf = 0;
+}
+
+int			check_pieces(t_struct *f, int **tab)
+{
+	init(f);
+	while (f->x1 < f->l + 1)
+	{
+		f->y1 = 4;
+		while (f->y1 < f->c + 4)
+		{
+			f->i = 0;
+			f->x2 = f->x1;
+			check1(f, tab);
+			if (f->i == f->lp && f->j == f->cp && f->tmp > 1000
+					&& f->tmp < 2000 && (f->a == 0 || (f->p == 1
+					&& f->a > f->tmp) || (f->p == 2 && f->a >= f->tmp)))
+			{
+				f->a = f->tmp;
+				f->lf = f->x1;
+				f->cf = f->y1;
+			}
+			f->tmp = 0;
+			f->y1++;
+		}
+		f->x1++;
+	}
 	return (1);
 }
